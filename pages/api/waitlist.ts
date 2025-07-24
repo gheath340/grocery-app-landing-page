73573aad07d4992a446815dd6848f340f633dec0
Garrett Heath <heathsmail0@gmail.com>
2025-07-24 16:49:52 -0600

    setup supabase

---
+++ pages/api/waitlist.ts
@@ -0,0 +1,31 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { supabase } from '../../lib/supabaseClient';
+
+type Data =
+  | { message: string }
+  | { error: string };
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  const { email } = req.body;
+
+  if (typeof email !== 'string' || !email.includes('@')) {
+    return res.status(400).json({ error: 'Invalid email' });
+  }
+
+  const { error } = await supabase
+    .from('waitlist_emails')
+    .insert([{ email }]);
+
+  if (error) {
+    return res.status(400).json({ error: error.message });
+  }
+
+  return res.status(200).json({ message: 'Successfully added to waitlist' });
+}
