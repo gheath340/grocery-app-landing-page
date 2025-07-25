1bcaa651ea1cf3bbe0f1afaae64ff185518bccee
Garrett Heath <heathsmail0@gmail.com>
2025-07-25 09:55:35 -0600

    removed unnecessary lines in subaBaseClient

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,12 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
-  throw new Error('Missing NEXT_PUBLIC_SUPABASE_URL env var');
-}
-if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
-  throw new Error('Missing NEXT_PUBLIC_SUPABASE_ANON_KEY env var');
-}
-
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
